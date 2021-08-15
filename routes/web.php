15d608be39f43eb912553d86f5b8c4fd79a1aacd
kissy <kissyalone@gmail.com>
2021-08-15 19:34:53 -0300

    consulta API, pagincao e alteracao nas rotas

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,14 @@
 use Illuminate\Support\Facades\Route;
 require __DIR__.'/auth.php';
 
-Route::get('/')->name('home')->uses('HomeController');
+//home page
+Route::get('/{page?}')->name('home')->uses('HomeController');
+Route::get('/personagem/{id}')->name('personagem.detalhe')->uses('HomeController@personagemDetalhe');
+
+//Meus personagens
 Route::get('/personagens')->name('personagem')->uses('PersonagensController')->middleware('auth');
+
+//Sobre
 Route::get('/sobre')->name('sobre')->uses('SobreController');
 
 
