2f719109e04bc3ecd7b9624b2e0590b94982a59c
Jhonata <kissyalone@gmail.com>
2021-08-18 15:41:36 -0300

    componentizando os cards da home de acordo request

--- app/Http/Controllers/PersonagensController.php
+++ app/Http/Controllers/PersonagensController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class PersonagensController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Personagem');
+        $personagens = Auth::user()->personagens()->orderByDesc('id')->paginate(2);
+        //dd($personagens,$personagens['personagem']);
+        return Inertia::render('Home', [
+            'personagens' => $personagens,
+            'paginacao' => null,
+            'routeName' => 'personagem.detalhe',
+            'title' => 'Rick and Morty - Meus Personagens'
+        ]);
     }
 }
